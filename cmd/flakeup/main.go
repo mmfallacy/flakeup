19d7c07d6be6b9c2a05abcce4eef37beaaa00872
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-10-21 02:56:09 +0800

    feat: add init subcommand and positional arguments

--- cmd/flakeup/main.go
+++ cmd/flakeup/main.go
@@ -5,25 +5,44 @@ import (
 	"github.com/integrii/flaggy"
 )
 
-func handleInit() {
-	fmt.Println("Hello from subcommand init")
-}
+var version = "0.0.1"
 
-func main() {
+var (
+	// Global flake flag
+	flake string
+	// Init subcommand
+	initCmd  *flaggy.Subcommand
+	template string
+)
+
+func init() {
 	flaggy.SetName("flakeup")
 	flaggy.SetDescription(`
 	flakeup is a supercharged 'nix flake init -t' that allows initializing
 	Nix flake projects from custom templates with advanced features like
 	conflict resolution and argument substitution.
 	`)
-
-	init := flaggy.NewSubcommand("init")
-	init.Description = "Initialize a new flake project from a template."
-
-	var flake string
 	flaggy.String(&flake, "f", "flake", `Specify the flake template source (e.g 'github:user/repo', '~/.flake') 
-		    Precedence: --flake flag > $FLAKEUP_FLAKE > $FLAKE > $HOME/.nixconfig.`)
 
-	flaggy.AttachSubcommand(init, 1)
-	flaggy.Parse()
+		    Precedence: --flake flag > $FLAKEUP_FLAKE > $FLAKE > $HOME/.nixconfig.`)
+	flaggy.SetVersion(version)
+	defer flaggy.Parse()
+
+	// Init Subcommand
+	initCmd = flaggy.NewSubcommand("init")
+	initCmd.Description = "Initialize a new flake project from a template."
+	flaggy.AttachSubcommand(initCmd, 1)
+
+	initCmd.AddPositionalValue(&template, "template", 1, true, "Name of the template to initialize.")
+
+}
+
+func handleInit() {
+	fmt.Println("Hello from subcommand init")
+}
+
+func main() {
+	if initCmd.Used {
+		handleInit()
+	}
 }
