1391804af449a4aebcf22f1d644eddb7574e1657
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-10-21 02:27:56 +0800

    feat: flakeup init subcommand

--- cmd/flakeup/main.go
+++ cmd/flakeup/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	// "flag"
+	"fmt"
+	"os"
+)
+
+var usage string = `
+Usage: flakeup [command]
+
+Commands:
+  init, i <template>    Initialize a new flake project from a template.
+
+Flags:
+  --flake <FLAKE>       Specify the flake template source (e.g., 'github:user/repo').
+                        Precedence: $FLAKEUP_FLAKE > $FLAKE > ~/.nixconfig > --flake flag.
+
+Description:
+  flakeup is a supercharged 'nix flake init -t' that allows initializing
+  Nix flake projects from custom templates with advanced features like
+  conflict resolution and argument substitution.
+`
+
+func handleInit() {
+	fmt.Println("Hello from subcommand init")
+}
 
 func main() {
-	fmt.Println("Hello World!")
+
+	if len(os.Args) < 2 {
+		fmt.Println("flakeup: Missing subcommand [init].")
+		fmt.Println(usage)
+		os.Exit(1)
+	}
+
+	subcmd := os.Args[1]
+
+	switch subcmd {
+	case "init":
+		handleInit()
+	default:
+		fmt.Printf("flakeup: Unknown subcommand %s\n", subcmd)
+		fmt.Println(usage)
+	}
+
 }
