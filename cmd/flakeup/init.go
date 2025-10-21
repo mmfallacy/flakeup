4d65a67a88e4c02d95b0581ab4016be19a48257e
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-10-22 02:50:41 +0800

    feat: draft getTemplate from given flake#flakeupTemplates

--- cmd/flakeup/init.go
+++ cmd/flakeup/init.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+)
+
+func prettify(v any) (string, error) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+func getTemplates() {
+	flakeArg := fmt.Sprintf("%s#flakeupTemplates", flake)
+	cmd := exec.Command("nix", "eval", flakeArg, "--json")
+
+	out, err := cmd.Output()
+
+	if err != nil {
+		panic("flakeup: nix eval failed to run!")
+	}
+
+	var templates TTemplates
+	if err := json.Unmarshal(out, &templates); err != nil {
+		panic("flakeup: nix eval failed to run!")
+	}
+
+	sout, err := prettify(templates)
+	if err != nil {
+		panic("")
+	}
+	fmt.Println("Unmarshalled: %s", sout)
+}
 
 func handleInit() {
-	fmt.Println("Hello from init subcommand!")
+	fmt.Printf("Cloning %s from %s\n", template, flake)
+	getTemplates()
 }
