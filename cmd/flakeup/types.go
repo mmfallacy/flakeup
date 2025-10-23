3584fc2f01e1670390bc38e0cb89e58b322cd435
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-10-24 02:10:41 +0800

    feat(types): turn all properties in json struct to pointers

--- cmd/flakeup/types.go
+++ cmd/flakeup/types.go
@@ -3,22 +3,22 @@ package main
 type TConflictAction string
 
 type TParameter struct {
-	Name string `json:"name"`
+	Name *string `json:"name"`
 	// Nullable
 	Prompt  *string `json:"prompt"`
 	Default *string `json:"default"`
 }
 
 type TRule struct {
-	OnConflict TConflictAction
+	OnConflict *TConflictAction
 }
 
 type TRules map[string]TRule
 
 type TTemplate struct {
-	Root       string       `json:"root"`
-	Parameters []TParameter `json:"parameters"`
-	Rules      TRules       `json:"rules"`
+	Root       *string       `json:"root"`
+	Parameters *[]TParameter `json:"parameters"`
+	Rules      *TRules       `json:"rules"`
 }
 
 type TTemplates map[string]TTemplate
