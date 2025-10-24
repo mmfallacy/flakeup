01f20e37d333842689338a4bc46aeafc0f543d37
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-10-25 01:54:56 +0800

    feat: refactor init to use nix.GetFlakeOutput

--- cmd/flakeup/init.go
+++ cmd/flakeup/init.go
@@ -1,40 +1,23 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os/exec"
 
 	"github.com/mmfallacy/flakeup/internal/nix"
 	"github.com/mmfallacy/flakeup/internal/utils"
 )
 
-func getTemplates() {
-	flakeArg := fmt.Sprintf("%s#flakeupTemplates", flake)
-	cmd := exec.Command("nix", "eval", flakeArg, "--json")
-
-	out, err := cmd.Output()
-
-	if err != nil {
-		utils.Panic("flakeup: nix eval failed to run!", err)
-	}
-
-	var templates TTemplates
-	if err := json.Unmarshal(out, &templates); err != nil {
-		utils.Panic("flakeup: failed to unmarshal json from nix eval", err)
-	}
-
-	sout, err := utils.Prettify(templates)
-	if err != nil {
-		utils.Panic("", err)
-	}
-
-	fmt.Println("Unmarshalled: %s", sout)
-}
-
 func handleInit() {
 	fmt.Println("Cloning %s from %s", template, flake)
-	hasOutput := nix.HasFlakeOutput(flake, "flakeupTemplates")
+	hasOutput, err := nix.HasFlakeOutput(flake, "flakeupTemplates")
+
+	if err != nil {
+		utils.Panic("Something went wrong", err)
+	}
+
 	fmt.Println("Does my flake have the proper output? %t", hasOutput)
-	getTemplates()
+
+	template, err := nix.GetFlakeOutput[TTemplates](flake, "flakeupTemplates")
+
+	fmt.Println("Got flake output:\n", utils.Prettify(template))
 }
