142249083c384e444df07526738dba6a2c93353a
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-10-29 23:55:52 +0800

    feat: user prompting

---
+++ internal/cli/ask.go
@@ -0,0 +1,47 @@
+package cli
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+var scanner = bufio.NewScanner(os.Stdin)
+
+var ErrAskInvalidAnswer = errors.New("answer is outside choices")
+
+func ask[T ~string](question string, choices []T) (T, error) {
+	prettyChoices := ""
+
+	for i, choice := range choices {
+		if i > 0 {
+			prettyChoices += "/"
+		}
+		prettyChoices += "[" + string(choice[:1]) + "]" + string(choice[1:])
+	}
+
+	fmt.Printf("%s: %s? ", question, prettyChoices)
+
+	if !scanner.Scan() {
+		return "", scanner.Err()
+	}
+
+	answer := strings.ToLower(strings.TrimSpace(scanner.Text()))
+
+	for _, choice := range choices {
+		if len(answer) < 1 {
+			return "", ErrAskInvalidAnswer
+		}
+		if answer == string(choice) {
+			return choice, nil
+		}
+		// match prefix
+		if len(choice) >= len(answer) && string(choice)[:len(answer)] == answer {
+			return choice, nil
+		}
+	}
+
+	return "", ErrAskInvalidAnswer
+}
