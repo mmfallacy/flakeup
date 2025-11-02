5a02db76b54a22cb7c391c357605e41b6e71ca47
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-11-03 02:24:38 +0800

    feat: copy functions

--- internal/core/copy.go
+++ internal/core/copy.go
@@ -6,7 +6,76 @@ import (
 	"os"
 )
 
-func Copy(src, dst string) error {
+// Assumption: file at dst already exists
+func CopyPrepend(src, dst string) error {
+	pre, err := os.Open(src)
+
+	if err != nil {
+		return err
+	}
+
+	defer pre.Close()
+
+	// Backup existing file
+	bak := fmt.Sprintf("%s.bak")
+	if err := os.Rename(dst, bak); err != nil {
+		return err
+	}
+
+	post, err := os.Open(bak)
+
+	if err != nil {
+		return err
+	}
+
+	defer post.Close()
+
+	out, err := os.OpenFile(dst, os.O_CREATE|os.O_EXCL, 0o644)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	if _, err = io.Copy(out, pre); err != nil {
+		return err
+	}
+
+	if _, err = io.Copy(out, post); err != nil {
+		return err
+	}
+
+	// Remove backup
+	os.Remove(bak)
+	return out.Sync()
+}
+
+// Assumption: file at dst already exists
+func CopyAppend(src, dst string) error {
+	in, err := os.Open(src)
+
+	if err != nil {
+		return err
+	}
+
+	defer in.Close()
+
+	out, err := os.OpenFile(dst, os.O_WRONLY|os.O_APPEND, 0o644)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	if _, err = io.Copy(out, in); err != nil {
+		return err
+	}
+
+	return out.Sync()
+}
+
+// Assumption: file at dst does not exist
+func CopyClean(src, dst string) error {
 	fmt.Println("Copying ", src, " to ", dst)
 	in, err := os.Open(src)
 
@@ -16,7 +85,7 @@ func Copy(src, dst string) error {
 
 	defer in.Close()
 
-	out, err := os.Create(dst)
+	out, err := os.OpenFile(dst, os.O_CREATE|os.O_EXCL, 0o644)
 
 	if err != nil {
 		return err
@@ -24,9 +93,33 @@ func Copy(src, dst string) error {
 
 	defer out.Close()
 
-	_, err = io.Copy(out, in)
+	if _, err = io.Copy(out, in); err != nil {
+		return err
+	}
+
+	return out.Sync()
+}
+
+// Assumption: file at dst exists
+func CopyOverwrite(src, dst string) error {
+	fmt.Println("Copying ", src, " to ", dst)
+	in, err := os.Open(src)
 
 	if err != nil {
+		return err
+	}
+
+	defer in.Close()
+
+	out, err := os.OpenFile(dst, os.O_WRONLY|os.O_TRUNC, 0o644)
+
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	if _, err = io.Copy(out, in); err != nil {
 		return err
 	}
 
