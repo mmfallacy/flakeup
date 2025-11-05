d4c12fb864054d511633d26812e2468d3b87297c
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-11-05 20:35:54 +0800

    feat: mergeinto

---
+++ internal/core/merge.go
@@ -0,0 +1,53 @@
+package core
+
+import (
+	"io"
+	"os"
+)
+
+// b is nil-able
+func MergeInto(a string, b *string, c string, overwrite bool) error {
+	pre, err := os.Open(a)
+
+	if err != nil {
+		return err
+	}
+
+	defer pre.Close()
+
+	// Presume file at c does not exist, throw otherwise
+	flags := os.O_CREATE | os.O_EXCL
+	if overwrite {
+		flags = os.O_CREATE | os.O_TRUNC
+	}
+	out, err := os.OpenFile(c, flags, 0o644)
+
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	if _, err = io.Copy(out, pre); err != nil {
+		return err
+	}
+
+	// When b is nil, no more stuff needs to be appended
+	if b == nil {
+		return out.Sync()
+	}
+
+	post, err := os.Open(*b)
+
+	if err != nil {
+		return err
+	}
+
+	defer post.Close()
+
+	if _, err = io.Copy(out, pre); err != nil {
+		return err
+	}
+
+	return out.Sync()
+}
