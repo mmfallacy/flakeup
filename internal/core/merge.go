e7f80860a30c2d4a61effd2b0764cb37bb2d17d6
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2025-12-13 01:09:42 +0800

    feat: merge with substitutions

--- internal/core/merge.go
+++ internal/core/merge.go
@@ -5,6 +5,44 @@ import (
 	"os"
 )
 
+func MergeIntoWithSubstitutions(a string, b *string, c string, sub Substitutions) error {
+	pre, err := os.Open(a)
+	if err != nil {
+		return err
+	}
+	defer pre.Close()
+
+	out, err := os.OpenFile(c, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0o644)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	presub := NewSubstituter(pre, sub)
+	if _, err = io.Copy(out, presub); err != nil {
+		return err
+	}
+
+	if b == nil {
+		return out.Sync()
+	}
+
+	// Append contents of b
+	post, err := os.Open(*b)
+	if err != nil {
+		return err
+	}
+	defer post.Close()
+
+	postsub := NewSubstituter(post, sub)
+	if _, err = io.Copy(out, postsub); err != nil {
+		return err
+	}
+
+	return out.Sync()
+
+}
+
 // b is nil-able
 func MergeInto(a string, b *string, c string) error {
 	pre, err := os.Open(a)
